cc42e2e6a6df4c9353ca77cb832f5798fe4ead9d
valma-GigabyteA16 <Github.valentin.martin@gmail.com>
2026-03-10 16:30:00 +0100

    modif : modification du fichier config.php

--- database/config.php
+++ database/config.php
@@ -1,15 +1,17 @@
 <?php
-$host = 'localhost';
-$dbname = 'cineverse_db';
-$username = 'root';
-$password = 'root';
+session_start();
+
+$host = getenv('DB_HOST') ?: 'mysql-server'; // Variable d'env ou nom du service
+$port = '3306';
+$dbname = getenv('DB_DATABASE') ?: 'cineverse_db';
+$username = getenv('DB_USER') ?: 'root';
+$password = getenv('DB_PASS') ?: '';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    // Définit le mode d'erreur PDO sur exception
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch(PDOException $e) {
     echo "Erreur de connexion : " . $e->getMessage();
 }
 ?>
-
